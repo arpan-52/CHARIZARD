186a488a4659e8bb9dab9901436fd7b322c87be3
Arpan Pal <arpan522000@gmail.com>
2025-12-30 19:16:26 -0700

    step_step

--- charizard/utils/selfcal_utils/prepare.py
+++ charizard/utils/selfcal_utils/prepare.py
@@ -79,7 +79,7 @@ mstransform(
     vis='{src_ms}',
     outputvis='{output_ms}',
     field='{field}',
-    datacolumn='CORRECTED_DATA',
+    datacolumn='CORRECTED',
     chanaverage=True,
     chanbin={freqbin},
     keepflags=True
