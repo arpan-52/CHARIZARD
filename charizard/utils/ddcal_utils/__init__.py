6805c05604e79f7fd0d62f6a03ebbdd860545e45
Arpan Pal <arpan522000@gmail.com>
2026-01-05 09:31:03 -0700

    step_step

--- charizard/utils/ddcal_utils/__init__.py
+++ charizard/utils/ddcal_utils/__init__.py
@@ -8,13 +8,13 @@ from .source_matcher import (
     load_pybdsf_catalog,
     find_bright_sources_and_write_regions
 )
-from .peeling import run_peeling_loop
+from .peeling import run_ddcal_peeling
 from .concat import concat_ms
 
 __all__ = [
     'run_pybdsf',
     'load_pybdsf_catalog',
     'find_bright_sources_and_write_regions',
-    'run_peeling_loop',
+    'run_ddcal_peeling',
     'concat_ms',
 ]
